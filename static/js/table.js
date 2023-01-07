27b4cac9bddaf14cff0dd5135e9be0b20234c10d
lein33 <53703703+lein33@users.noreply.github.com>
2023-01-06 21:15:05 -0500

    Add files via upload

---
+++ static/js/table.js
@@ -0,0 +1,8 @@
+$(document).ready( function () {
+    $('#myTable').DataTable({
+        responsive:true,
+        "language": {
+            "url": "https://cdn.datatables.net/plug-ins/1.13.1/i18n/es-ES.json"
+            }
+    });
+});
